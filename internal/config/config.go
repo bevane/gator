458db870ca1f5401c1ca36efb0701795840c0970
bevane <bevane@live.com>
2025-02-15 12:11:30 +0700

    feat: set user on config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	DbUrl           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() Config {
+	config := Config{}
+	configFilePath := getConfigFilePath()
+	data, _ := os.ReadFile(configFilePath)
+	json.Unmarshal(data, &config)
+	return config
+}
+
+func (c *Config) SetUser(username string) {
+	configFilePath := getConfigFilePath()
+	c.CurrentUserName = username
+	data, _ := json.Marshal(c)
+	os.WriteFile(configFilePath, data, 0666)
+}
+
+func getConfigFilePath() string {
+	homeDir, _ := os.UserHomeDir()
+	configFilePath := filepath.Join(homeDir, "gatorconfig.json")
+	return configFilePath
+}
--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/bevane/gator/internal/config"
+)
+
 func main() {
+	configFile := config.Read()
+	configFile.SetUser("bevane")
+	updatedConfigFile := config.Read()
+	fmt.Println(updatedConfigFile)
 }
